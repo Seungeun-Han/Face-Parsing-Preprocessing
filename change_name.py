a9f04f32b4381595a33aeecc9d4b50729df18823
Seungeun Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-09-07 14:52:36 +0900

    Update change_name.py

--- change_name.py
+++ change_name.py
@@ -1,23 +1,29 @@
 import os
 
-upper_path = os.path.join('D:/Dataset/CelebAMask-HQ/necklace_CelebAMask-HQ_473_HoriFlip')
-# type_list = os.listdir(upper_path) # ['edges', 'images', 'cat_edges', 'labels']
-type_list = ['edges', 'images', 'labels', 'labels_necklace_Last']
+upper_path = os.path.join(r'D:\Dataset\230703_tagging_samples\CCTV_DB_Dataset\annotations')
 
-for type in type_list:
-    image_path = os.path.join(upper_path, type)
-    image_list = os.listdir(image_path)
-    for image in image_list:
-        # print(os.path.join(image_path, image))
-        # print(os.path.join(image_path, image[:-4] + "_flip+crop256" + image[-4:]))
-        # print(image[:-4] + "_kf94" + image[-4:])
-        print(image[:-4] + "_HoriFlip" + image[-4:])
-        os.renames(os.path.join(image_path, image), os.path.join(image_path, image[:-4] + "_HoriFlip" + image[-4:]))
-    print("end")
+# type_list = os.listdir(upper_path)
+# for type in type_list:
+#     image_path = os.path.join(upper_path, type)
+#     image_list = os.listdir(image_path)
+#     for image in image_list:
+#         if "llip" in image:
+#             # print(os.path.join(image_path, image))
+#             # print(os.path.join(image_path, image[:-4] + "_flip+crop256" + image[-4:]))
+#             print(image)
+#             # print('_'.join(image.split('_')[:-1])+"_l_eye.png")
+#
+#             os.renames(os.path.join(image_path, image), os.path.join(image_path, '_'.join(image.split('_')[:-1]) + "_l_lip.png"))
 
-"""image_list = os.listdir(upper_path)
+    # print("end")
+
+image_list = os.listdir(upper_path)
 for image in image_list:
-    if "kf94" not in image:
-        # print(image, image[:-4] + "_kf94" + image[-4:])
-        os.renames(os.path.join(upper_path, image), os.path.join(upper_path, image[:-4] + "_kf94" + image[-4:]))
-print("end")"""
+    # if "hand" not in image:
+    #     # print(image, image[:-4] + "_kf94" + image[-4:])
+    #     os.renames(os.path.join(upper_path, image), os.path.join(upper_path, image[:-4]+"_hand"+image[-4:]))
+    if "r-ear" in image:
+        print(image)
+        # print('_'.join(image.split('_')[:-1]) + "_l_lip.png")
+        os.renames(os.path.join(upper_path, image), os.path.join(upper_path, '_'.join(image.split('_')[:-1]) + "_r_ear.png"))
+print("end")
