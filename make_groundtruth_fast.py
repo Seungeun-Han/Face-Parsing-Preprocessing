0db7f2b18b9e0086096ea0f2da2fa1bf347fa7e2
Seungeun Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-09-07 16:17:32 +0900

    Update make_groundtruth_fast.py

--- make_groundtruth_fast.py
+++ make_groundtruth_fast.py
@@ -1,11 +1,29 @@
+# Copyright (c) 한승은. All rights reserved.
+
 import os
 import numpy as np
 import cv2
 import time
 
+IMAGE_PATH = 'D:/Dataset/CelebAMask-HQ/CelebAMask-HQ/CelebA-HQ-img/'
+ANNOTATIOM_PATH = 'D:/Dataset/CelebAMask-HQ/CelebAMask-HQ/CelebAMask-HQ-mask-anno_acc'
+SAVE_PATH = "D:/Dataset/CelebAMask-HQ/CelebA-HQ_512/labels"
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
+INPUT_SIZE = 512
+
+# Original
+# LABELS = {'bg': 0, 'skin': 1, 'nose': 2, 'eye_g': 3, 'l_eye': 4, 'r_eye': 5,
+#         'l_brow': 6, 'r_brow': 7, 'l_ear': 8, 'r_ear': 9, 'mouth': 10, 'u_lip': 11,
+#         'l_lip': 12, 'hair': 13, 'hat': 14, 'ear_r': 15, 'neck_l': 16, 'neck': 17, 'cloth': 18}
+
+
+# # Necklace Last
 LABELS = {'bg': 0, 'skin': 1, 'nose': 2, 'eye_g': 3, 'l_eye': 4, 'r_eye': 5,
         'l_brow': 6, 'r_brow': 7, 'l_ear': 8, 'r_ear': 9, 'mouth': 10, 'u_lip': 11,
-        'l_lip': 12, 'hair': 13, 'hat': 14, 'ear_r': 15, 'neck_l': 16, 'neck': 17, 'cloth': 18}
+        'l_lip': 12, 'hair': 13, 'hat': 14, 'ear_r': 15, 'neck': 16, 'cloth': 17, 'neck_l': 18}
+
+
 
 def make_groundTruth(label, seg, seg_name):
     """
@@ -43,10 +61,10 @@ def make_groundTruth(label, seg, seg_name):
         label[0][seg_list] = LABELS[seg_name]#*10
 
     elif seg_name == 'skin':  # cond1, 11
-        skin_idx = [x for x in range(len(label[0])) if label[0][x] == 0 or label[0][x] == LABELS['ear_r']]
+        skin_idx = [x for x in range(len(label[0])) if label[0][x] == 0 or label[0][x] == LABELS['ear_r']] #  or label[0][x] == LABELS['ear_r']
         intersection = list(set(seg_list) & set(skin_idx))
         label[0][intersection] = LABELS[seg_name]# * 10
-    elif seg_name == 'l_ear' or seg_name == 'r_ear':  # cond2
+    elif seg_name == 'l_ear' or seg_name == 'r_ear':  # cond2, 10
         ear_idx = [x for x in range(len(label[0])) if label[0][x] != LABELS['hair'] and label[0][x] != LABELS['ear_r']]  # *10
         intersection = list(set(seg_list) & set(ear_idx))
         label[0][intersection] = LABELS[seg_name]# * 10
@@ -66,7 +84,7 @@ def make_groundTruth(label, seg, seg_name):
         hair_idx = [x for x in range(len(label[0])) if label[0][x] != LABELS['eye_g'] and label[0][x] != LABELS['ear_r']]  # *10
         intersection = list(set(seg_list) & set(hair_idx))
         label[0][intersection] = LABELS[seg_name] #* 10
-    elif seg_name == 'nose':  # cond8, 12
+    elif seg_name == 'nose':  # cond8
         hair_idx = [x for x in range(len(label[0])) if label[0][x] != LABELS['hair'] and label[0][x] != LABELS['eye_g']]  # *10
         intersection = list(set(seg_list) & set(hair_idx))
         label[0][intersection] = LABELS[seg_name] #* 10
@@ -74,30 +92,42 @@ def make_groundTruth(label, seg, seg_name):
     label = np.reshape(label, (h, -1))
     return label
 
-im_path = os.path.join('./images/')
-image_list = os.listdir(im_path)
+# Image List
+image_list = os.listdir(IMAGE_PATH)
+# 일부만 만들고 싶을 때는 아래 코드 사용
+# image_list = [x for x in image_list if int(x[:-4]) < 10000] #  >= 10000 and int(x[:-4])
 
-parsing_anno_path = os.path.join('./labels_sep/')
-annotation_list = os.listdir(parsing_anno_path)
+# Annotation List
+annotation_list = os.listdir(ANNOTATIOM_PATH)
+annotation_name_list = [i[:5] for i in annotation_list]  # CelebAMask-HQ Only
+annotation_name_list = list(set(annotation_name_list))
 
-save_dir = "./labels/" #fast_labels
+# If labels have already been created, exclude them from the task.
+saved_list = os.listdir(SAVE_PATH)
+image_list = [i for i in image_list if i[:-4].zfill(5) + ".png" not in saved_list]
+print(len(image_list))
+
+
 for im_list in image_list:
     start = time.time()
-    parent_img_name = im_list[:-4].zfill(5)
+    parent_img_name = im_list[:-4].zfill(5)  # 0.jpg --> 00000.jpg
     print(parent_img_name)
 
-    label = np.zeros((512, 512))
-    for idx, ann_list in enumerate(annotation_list):
-        if parent_img_name in ann_list:
-            annotation_path = parsing_anno_path + ann_list
-            parsing_anno = cv2.imread(annotation_path, cv2.IMREAD_GRAYSCALE)
-            #print(ann_list[6:-4])
-            label = make_groundTruth(label, parsing_anno, ann_list[6:-4])
+    label = np.zeros((INPUT_SIZE, INPUT_SIZE))
 
-    label = cv2.resize(label, (473, 473), cv2.INTER_NEAREST)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    cv2.imwrite(save_dir + parent_img_name + ".png", label)
+    if parent_img_name in annotation_name_list:
+        part_list = [i for i in annotation_list if parent_img_name in i]
+        print(part_list)
+        for p in part_list:
+            annotation_path = os.path.join(ANNOTATIOM_PATH, p)
+
+            parsing_anno = cv2.imread(annotation_path, cv2.IMREAD_GRAYSCALE)
+            parsing_anno = cv2.resize(parsing_anno, (INPUT_SIZE, INPUT_SIZE), cv2.INTER_NEAREST)
+
+            label = make_groundTruth(label, parsing_anno, p[6:-4])  # CelebAMask-HQ Only
+
+
+    cv2.imwrite(os.path.join(SAVE_PATH, parent_img_name + ".png"), label)
     print("time :", time.time() - start)
 
 
