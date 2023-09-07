96843b5f0fc39d6de4f22c2fd7df9e1b3ae700a6
Seungeun Han <101082685+Seungeun-Han@users.noreply.github.com>
2023-09-07 16:05:47 +0900

    Update Write_TXT_List.py

--- Write_TXT_List.py
+++ Write_TXT_List.py
@@ -1,12 +1,14 @@
+# Copyright (c) 한승은. All rights reserved.
+
 import os
 
-img_path = r'D:\Dataset\CelebAMask-HQ\CelebA-HQ_256_forsending\valid\images'  # train / valid / test
+img_path = r'./CelebAMask-HQ/valid/images'  # train / valid / test
 
 img_list = os.listdir(img_path)
 
 print(len(img_list))
 
-txt_path = r"D:\Dataset\CelebAMask-HQ\CelebA-HQ_256_forsending/valid_list.txt"   #  train / valid_list / test_list
+txt_path = r"./CelebAMask-HQ/valid_list.txt"   #  train / valid_list / test_list
 
 file = open(txt_path, 'w')
 
